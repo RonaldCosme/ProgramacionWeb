fadc133db60140d8a3a96da76594712d07fd1b7a
Ronald cosme monago <u201920185@upc.edu.pe>
2022-03-25 20:45:47 -0500

    acyualizacion

--- Sem1/src/main/java/pe/edu/upc/Program.java
+++ Sem1/src/main/java/pe/edu/upc/Program.java
@@ -1,10 +1,17 @@
 package pe.edu.upc;
 
-public class Program {
+public class Program
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Student Student=new Student();
+		
+		Student.setFirstName("ronald cosme");
+		Student.setId(1L);
+	
 	}
+	
 
 }
--- Sem1/src/main/java/pe/edu/upc/Student.java
+++ Sem1/src/main/java/pe/edu/upc/Student.java
@@ -1,5 +1,24 @@
 package pe.edu.upc;
 
 public class Student {
+	
+	
+	private long id;
+	private string firstName;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public string getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(string firstName) {
+		this.firstName = firstName;
+	}
 
+	
 }
